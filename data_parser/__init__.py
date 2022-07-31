13e064e1cce5df190e08f8747dda8292f57f24d3
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-07-31 02:40:55 +0200

    Added parsing of comment IDs

---
+++ data_parser/__init__.py
@@ -0,0 +1,37 @@
+from github import Github
+import base64
+
+
+# TODO: Implement
+def parse_comment_ids(
+        comment_id_source_repo: str,
+        github_access_token: str,
+        paths_to_data: list[str]
+) -> dict:
+    """
+    Parses the VoynaSlov repository and returns the comment ids
+    :param comment_id_source_repo: Reference to a Github repo with comment IDs
+    :param github_access_token: Github access token
+    :param paths_to_data: List of paths inside the repo that store the data
+    :return: comment_ids
+    """
+    comment_ids = {}
+    g = Github(github_access_token)
+    repo = g.get_repo(comment_id_source_repo)
+    for path in paths_to_data:
+        contents = repo.get_contents(path)
+        while contents:
+            cur_file = contents.pop(0)
+            if cur_file.type == "dir":
+                contents.extend(repo.get_contents(cur_file.path))
+            else:
+                content = cur_file.content
+                content_str = base64.b64decode(content).decode('utf-8')
+                comment_ids[cur_file.path] = content_str.split('\n')
+    return comment_ids
+
+
+# TODO: Implement
+def parse_comment_data():
+    # Gets comment data from VK based on comment IDs
+    return
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from code.data_parser import parse_comment_ids, parse_comment_data
+from dotenv import dotenv_values
+from data_parser import parse_comment_ids, parse_comment_data
+
+config = dotenv_values(".env")
 
 if __name__ == '__main__':
-    parse_comment_ids()
+    comment_ids = parse_comment_ids(
+        comment_id_source_repo=config['COMMENT_ID_SOURCE_REPO'],
+        github_access_token=config['GITHUB_ACCESS_TOKEN'],
+        paths_to_data=[
+            'VK/public-reaction/independent',
+            'VK/public-reaction/state-affiliated'
+        ]
+    )
     parse_comment_data()
