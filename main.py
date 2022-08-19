49c4a154666db03684f254a964c9cbd62e707404
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-08-19 02:30:35 +0200

    Added similarity function

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import multiprocessing as mp
 
 from data_parser import parse_comment_data
 from database_adapter import write_comment_to_db
+from models.graph_based_approach import enrich_users_data, get_similarity
 
 config = dotenv_values(".env")
 if not config:
@@ -26,9 +27,17 @@ db_client = pymongo.MongoClient(f"mongodb+srv://"
                                 tlsAllowInvalidCertificates=True)
 
 if __name__ == '__main__':
-    pool = mp.Pool(mp.cpu_count())
+    # pool = mp.Pool(mp.cpu_count())
     # insert_comment_ids(db_client, api)
-    for comment in parse_comment_data(db_client, api):
-        # write_comment_to_db(comment, db_client)
-        pool.apply_async(write_comment_to_db, args=(comment, db_client))
-    pool.close()
+    # for comment in parse_comment_data(db_client, api):
+    # write_comment_to_db(comment, db_client)
+    # pool.apply_async(write_comment_to_db, args=(comment, db_client))
+    # pool.close()
+    #     The idea for testing the models is to test on a small sample (e.g. 10) of comment/user ids
+    # Then, after collecting all the data, we can run the models remotely on a server on all the dataset
+    # enrich_users_data(db_client)
+    users = db_client.dataVKnodup.users.find({'enriched': True})
+    for user1 in users:
+        for user2 in users:
+            if user1 != user2:
+                print(get_similarity((user1, user2)))
--- models/graph_based_approach.py
+++ models/graph_based_approach.py
@@ -8,7 +8,8 @@ Steps:
     - comment rate: number of all comments by user in db
     - deactivated - not used as a feature but used in
       cluster analysis later on. Already present in the db.
-2. Write user similarity function. Different for:
+2. Write user similarity function - DONE
+   Different for:
     - nominal data type
     - real data type
 3. Construct multi-attributed graph Gm
@@ -21,19 +22,26 @@ Steps:
 """
 import pymongo  # type: ignore
 from data_parser import get_foaf_data, get_activity_count
+from typing import Tuple
+from datetime import datetime
 
 
 def enrich_users_data(
-        db_client: pymongo.MongoClient
+        db_client: pymongo.MongoClient,
 ) -> None:
     users = db_client.dataVKnodup.users.find({'enriched': {'$ne': True}})
-    for user in users:
+    for i, user in enumerate(users):
         foaf = get_foaf_data(user['vk_id'])
         activity = get_activity_count(user['vk_id'], db_client)
+        if foaf['created_at']:
+            vk_age = (datetime(2022, 8, 1, 0, 0, 0) - foaf['created_at']).days
+        else:
+            vk_age = None
         db_client.dataVKnodup.users.update_one(
             {'_id': user['_id']},
             {'$set': {
                 'created_at': foaf['created_at'],
+                'vk_age': vk_age,
                 'timezone': foaf['timezone'],
                 'followee_rate': foaf['followee_rate'],
                 'follower_rate': foaf['follower_rate'],
@@ -42,3 +50,37 @@ def enrich_users_data(
                 'enriched': True
             }}
         )
+
+
+def get_similarity(
+        users: Tuple
+) -> float:
+    features = {
+        'vk_age': 'real',
+        'timezone': 'nominal',
+        'followee_rate': 'real',
+        'follower_rate': 'real',
+        'follower_to_followee': 'real',
+        'comment_rate': 'real'
+    }
+    similarities = []
+    for feature, typ in features.items():
+        if users[0][feature] and users[1][feature]:
+            if typ == 'real':
+                similarities.append(get_real_similarity((users[0][feature], users[1][feature])))
+            elif typ == 'nominal':
+                similarities.append(get_nominal_similarity((users[0][feature], users[1][feature])))
+    avg_similarity = sum(similarities) / len(similarities)
+    return avg_similarity
+
+
+def get_nominal_similarity(
+        values: Tuple
+) -> bool:
+    return values[0] == values[1]
+
+
+def get_real_similarity(
+        values: Tuple
+) -> float:
+    return 1 / (1 + abs(values[0] - values[1]))
