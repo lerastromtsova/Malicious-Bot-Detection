d7afc4555069be0c92d3706e840191f8cda6a4fa
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-08-24 18:43:26 +0200

    Added enriching user data

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import logging
 import vk  # type: ignore
 import pymongo  # type: ignore
 import os
-import multiprocessing as mp
 import sys
 
-from data_parser import parse_comment_data
-from database_adapter import write_comment_to_db
-from models.graph_based_approach import enrich_users_data, get_similarity
+from models.graph_based_approach import enrich_users_data
 
 config = dotenv_values(".env")
 if not config:
@@ -28,18 +25,4 @@ db_client = pymongo.MongoClient(f"mongodb+srv://"
                                 tlsAllowInvalidCertificates=True)
 
 if __name__ == '__main__':
-    while True:
-        parse_comment_data(db_client, api, write_comment_to_db)
-
-# #
-# var i = 0;
-# var comment;
-# var comments = [];
-# var comment_ids = [797550];
-# var media_ids = [-11982368];
-# while (i != 1) {
-#     comment = API.wall.getComment({"owner_id": (media_ids[i]), "comment_id": (comment_ids[i]), "v": 5.131, "extended": 1});
-#     comments.push(comment);
-#     i = i + 1;
-# };
-# return comments;
+    enrich_users_data(db_client)
