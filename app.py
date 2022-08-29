f8f612b8d23890b5405a470cfe9d8e133ab425e7
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-08-29 23:51:53 +0200

    Added localization

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import pymongo
 from dotenv import dotenv_values
 from flask import Flask, render_template, request
+from flask_babel import Babel
 
 from database_adapter import get_user_data, get_comments_by_user
 
 app = Flask(__name__)
+babel = Babel(app)
 
 config = dotenv_values(".env")
 db_client = pymongo.MongoClient(f"mongodb+srv://"
@@ -14,6 +16,7 @@ db_client = pymongo.MongoClient(f"mongodb+srv://"
                                 tls=True,
                                 tlsAllowInvalidCertificates=True)
 
+
 @app.route("/search")
 def index():
     if request.args:
@@ -22,3 +25,8 @@ def index():
         comments = get_comments_by_user(db_client, user_id)
         return render_template('index.html', users=users, comments=comments)
     return render_template('index.html')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['en', 'ru'])
