2f07b56e20dfcad309f485720a77be28215c32da
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-09-01 00:51:24 +0200

    Added checking verified users

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import sys
+import time
 
 import pymongo  # type: ignore
 import vk  # type: ignore
@@ -29,14 +30,29 @@ db_client = pymongo.MongoClient(f"mongodb+srv://"
 
 if __name__ == '__main__':
     # friends_of_friends = get_friends_of_friends(db_client, api)
-    with open('friends.json', 'r') as f:
-        friends_of_friends = json.load(f)
-    users = db_client.dataVKnodup.users.find({}, {'vk_id': 1, '_id': 0})
-    users_set = set(u['vk_id'] for u in users)
-    my_friends = set([int(f) for f in friends_of_friends.keys()])
-    flattened_friends = [element for sublist in friends_of_friends.values() if sublist for element in sublist]
-    other_friends = set(flattened_friends)
-    my_friends_in_db = users_set.intersection(my_friends)
-    other_friends_in_db = users_set.intersection(other_friends)
-    print(len(my_friends_in_db), my_friends_in_db)
-    print(len(other_friends_in_db), other_friends_in_db)
+    # with open('friends.json', 'r') as f:
+    #     friends_of_friends = json.load(f)
+    users = db_client.dataVKnodup.users.find({'verified': {'$exists': 0}}, {'vk_id': 1, '_id': 0})
+    # users_set = set(u['vk_id'] for u in users)
+    # my_friends = set([int(f) for f in friends_of_friends.keys()])
+    # flattened_friends = [element for sublist in friends_of_friends.values() if sublist for element in sublist]
+    # other_friends = set(flattened_friends)
+    # my_friends_in_db = users_set.intersection(my_friends)
+    # other_friends_in_db = users_set.intersection(other_friends)
+    # # 3 {35738768, 1985833, 1058786}
+    # print(len(my_friends_in_db), my_friends_in_db)
+    # # 308
+    # print(len(other_friends_in_db), other_friends_in_db)
+    for user in users:
+        time.sleep(0.3)
+        user_info = api.users.get(
+            user_id=user['vk_id'],
+            fields='verified',
+            v='5.131'
+        )[0]
+        if 'verified' in user_info:
+            db_client.dataVKnodup.users.update_one(
+                {'vk_id': user['vk_id']},
+                {'$set': {'verified': user_info['verified']}}
+            )
+    db_client.close()
